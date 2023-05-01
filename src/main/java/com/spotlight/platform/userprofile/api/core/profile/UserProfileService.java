bb7abd80ae6d7f4834f351208de709d7a8e1202c
Nicholas Dutcher <19955985+NicholasDutcher@users.noreply.github.com>
2023-05-01 17:25:25 +0200

    Implement CommandTypes and API Calls Functionality

--- src/main/java/com/spotlight/platform/userprofile/api/core/profile/UserProfileService.java
+++ src/main/java/com/spotlight/platform/userprofile/api/core/profile/UserProfileService.java
@@ -1,21 +1,109 @@
 package com.spotlight.platform.userprofile.api.core.profile;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+
 import com.spotlight.platform.userprofile.api.core.exceptions.EntityNotFoundException;
 import com.spotlight.platform.userprofile.api.core.profile.persistence.UserProfileDao;
 import com.spotlight.platform.userprofile.api.model.profile.UserProfile;
 import com.spotlight.platform.userprofile.api.model.profile.primitives.UserId;
-
-import javax.inject.Inject;
+import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfilePropertyName;
+import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfilePropertyValue;
+import com.spotlight.platform.userprofile.api.web.resources.UserProfileApiModel;
 
 public class UserProfileService {
     private final UserProfileDao userProfileDao;
 
     @Inject
-    public UserProfileService(UserProfileDao userProfileDao) {
+    public UserProfileService(final UserProfileDao userProfileDao) {
         this.userProfileDao = userProfileDao;
     }
 
-    public UserProfile get(UserId userId) {
+    public UserProfile get(final UserId userId) {
         return userProfileDao.get(userId).orElseThrow(EntityNotFoundException::new);
     }
+
+    public UserProfile createOrUpdate(final UserProfileApiModel userProfileApiModel) {
+        switch (UserProfileValidator.getValidCommandType(userProfileApiModel)) {
+            case "replace":
+                return replace(userProfileApiModel);
+            case "increment":
+                return increment(userProfileApiModel);
+            case "collect":
+                return collect(userProfileApiModel);
+            default:
+                return null;
+        }
+    }
+
+    private UserProfile replace(final UserProfileApiModel userProfileApiModel) {
+        final var apiModelproperties = getApiModelProperties(userProfileApiModel);
+        for (final var property : userProfileApiModel.userProfileProperties().entrySet()) {
+            final var propertyKey = property.getKey();
+            apiModelproperties.put(propertyKey,
+                    UserProfileValidator.getValidPropertyValue(propertyKey, property.getValue()));
+        }
+        return creatUserProfile(userProfileApiModel.userId(), apiModelproperties);
+    }
+
+    private UserProfile increment(final UserProfileApiModel userProfileApiModel) {
+        final var apiModelproperties = getApiModelProperties(userProfileApiModel);
+        for (final var property : userProfileApiModel.userProfileProperties().entrySet()) {
+            final var propertyKey = property.getKey();
+            if (UserProfileValidator.isValidIncrementableProperty(propertyKey)) {
+                if (!apiModelproperties.containsKey(propertyKey)) {
+                    apiModelproperties.put(propertyKey, UserProfilePropertyValue.valueOf(0));
+                }
+                apiModelproperties.put(propertyKey,
+                        UserProfilePropertyValue.valueOf(
+                                UserProfileValidator.getValidIncrementableValue(apiModelproperties.get(propertyKey))
+                                        + UserProfileValidator
+                                                .getValidIncrementableValue(property.getValue())));
+            }
+        }
+        return creatUserProfile(userProfileApiModel.userId(), apiModelproperties);
+    }
+
+    private UserProfile collect(final UserProfileApiModel userProfileApiModel) {
+        final var apiModelproperties = getApiModelProperties(userProfileApiModel);
+        for (final var property : userProfileApiModel.userProfileProperties().entrySet()) {
+            final var propertyKey = property.getKey();
+            if (UserProfileValidator.isValidCollectableProperty(propertyKey)) {
+                if (!apiModelproperties.containsKey(propertyKey)) {
+                    apiModelproperties.put(propertyKey, UserProfilePropertyValue.valueOf(new ArrayList<String>()));
+                }
+                final var validValues = UserProfileValidator.getValidCollectableValue(
+                        apiModelproperties.get(propertyKey));
+                validValues.addAll(UserProfileValidator.getValidCollectableValue(property.getValue()));
+                apiModelproperties.put(propertyKey, UserProfilePropertyValue.valueOf(validValues));
+            }
+        }
+        return creatUserProfile(userProfileApiModel.userId(), apiModelproperties);
+    }
+
+    private Map<UserProfilePropertyName, UserProfilePropertyValue> getApiModelProperties(
+            final UserProfileApiModel userProfileApiModel) {
+        return new HashMap<>(getUserProfileFromDaoOrCreateWhenMissing(userProfileApiModel).userProfileProperties());
+    }
+
+    private UserProfile getUserProfileFromDaoOrCreateWhenMissing(final UserProfileApiModel userProfileApiModel) {
+        try {
+            return get(userProfileApiModel.userId());
+        } catch (final EntityNotFoundException e) {
+            return creatUserProfile(userProfileApiModel.userId(), new HashMap<>());
+        }
+    }
+
+    private UserProfile creatUserProfile(final UserId userId,
+            final Map<UserProfilePropertyName, UserProfilePropertyValue> userProfileProperties) {
+        final UserProfile newUserProfile = new UserProfile(
+                UserProfileValidator.getValidUserId(userId), Instant.now(), userProfileProperties);
+        userProfileDao.put(newUserProfile);
+        return newUserProfile;
+    }
+
 }
--- src/main/java/com/spotlight/platform/userprofile/api/web/resources/UserResource.java
+++ src/main/java/com/spotlight/platform/userprofile/api/web/resources/UserResource.java
@@ -1,19 +1,20 @@
 package com.spotlight.platform.userprofile.api.web.resources;
 
-import com.spotlight.platform.userprofile.api.core.profile.UserProfileService;
-import com.spotlight.platform.userprofile.api.model.profile.UserProfile;
-import com.spotlight.platform.userprofile.api.model.profile.primitives.UserId;
-
 import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/users/{userId}")
+import com.spotlight.platform.userprofile.api.core.profile.UserProfileService;
+import com.spotlight.platform.userprofile.api.model.profile.UserProfile;
+import com.spotlight.platform.userprofile.api.model.profile.primitives.UserId;
+
+@Path("/users/")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
@@ -21,13 +22,20 @@ public class UserResource {
     private final UserProfileService userProfileService;
 
     @Inject
-    public UserResource(UserProfileService userProfileService) {
+    public UserResource(final UserProfileService userProfileService) {
         this.userProfileService = userProfileService;
     }
 
-    @Path("profile")
+    @Path("{userId}/profile")
     @GET
-    public UserProfile getUserProfile(@Valid @PathParam("userId") UserId userId) {
+    public UserProfile getUserProfile(@Valid @PathParam("userId") final UserId userId) {
         return userProfileService.get(userId);
     }
+
+    @Path("update")
+    @POST
+    public UserProfile replaceUserProfileProperties(final UserProfileApiModel userProfile) {
+        return userProfileService.createOrUpdate(userProfile);
+    }
+
 }
